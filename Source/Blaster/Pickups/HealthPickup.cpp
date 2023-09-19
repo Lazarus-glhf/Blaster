87b63cb64739ffecc16157aee1194ea400d56c4f
Lazarus-glhf <redrumlivedog@outlook.com>
2023-09-19 20:47:05 +0800

    Set up health pickup

---
+++ Source/Blaster/Pickups/HealthPickup.cpp
@@ -0,0 +1,47 @@
+﻿#include "HealthPickup.h"
+#include "Blaster/Character/BlasterCharacter.h"
+#include "Blaster/BlasterComponent/BuffComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NiagaraComponent.h"
+
+
+AHealthPickup::AHealthPickup()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
+
+	PickupEffectComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("PickupEffectComponent"));
+	PickupEffectComponent->SetupAttachment(RootComponent);
+}
+
+void AHealthPickup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AHealthPickup::OnSphereOverlap(UPrimitiveComponent* OverlapComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool FromSweep, const FHitResult& SweepResult)
+{
+	Super::OnSphereOverlap(OverlapComponent, OtherActor, OtherComp, OtherBodyIndex, FromSweep, SweepResult);
+	
+	if (const ABlasterCharacter* BlasterCharacter = Cast<ABlasterCharacter>(OtherActor); BlasterCharacter)
+	{
+		
+	}
+	Destroy();
+}
+
+void AHealthPickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AHealthPickup::Destroyed()
+{
+	if (PickupEffect)
+	{
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, PickupEffect, GetActorLocation(), GetActorRotation());	
+	}
+	Super::Destroyed();
+}
+
---
+++ Source/Blaster/Pickups/HealthPickup.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickup.h"
+#include "HealthPickup.generated.h"
+
+UCLASS()
+class BLASTER_API AHealthPickup : public APickup
+{
+	GENERATED_BODY()
+
+public:
+	AHealthPickup();
+	virtual void Tick(float DeltaTime) override;
+	virtual void Destroyed() override;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlapComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool FromSweep, const FHitResult& SweepResult) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	float HealAmount = 50.f;
+
+	UPROPERTY(EditAnywhere)
+	float HealingTime = 5.f;
+
+	UPROPERTY(VisibleAnywhere)
+	class UNiagaraComponent* PickupEffectComponent;
+
+	UPROPERTY(EditAnywhere)
+	class UNiagaraSystem* PickupEffect;
+};
